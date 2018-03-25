ce150662eca25f929b33b62b11eb35e0f1f49aeb
Henry <Stormaggedon94@gmail.com>
2018-03-25 21:27:27 +0800

    Fixed Add Team Added Delete Team

--- OKare/OKareApp/urls/admin.py
+++ OKare/OKareApp/urls/admin.py
@@ -12,5 +12,23 @@ urlpatterns = [
     url(r'^get_patient_tasks/', Admin.getPatientTasks, name="Get_Patient_Tasks"),
     url(r'^addTask/', Admin.addTask, name="Add_Task"),
     url(r'^get_category_pie_data/', Admin.getCatData, name="Get_Cat_Data"),
+<<<<<<< Updated upstream
     url(r'^get_manage_team_info/', ManageTeam.returnteaminfo, name="ManageTeam_returnteaminfo")
+=======
+    url(r'^get_manage_team_info/', ManageTeam.returnteaminfo, name="ManageTeam_returnteaminfo"),
+    url(r'^get_nurse_with_no_team_info/', ManageTeam.returnnursewithnoteam, name="ManageTeam_nursewithnoteam"),
+    url(r'^remove_nurse_from_team/', ManageTeam.removenursefromteam, name="ManageTeam_removenursefromteam"),
+    url(r'^remove_patient_from_team/', ManageTeam.removepatientfromteam, name="ManageTeam_removepatientfromteam"),
+    url(r'^add_nurse_to_team/', ManageTeam.addnursetoteam, name="ManageTeam_addnursetoteam"),
+    url(r'^get_patient_in_team/', ManageTeam.getpatientinteam, name="ManageTeam_getpatientinteam"),
+    url(r'^get_patient_with_no_team_info/', ManageTeam.getpatientwithnoteaminfo, name="ManageTeam_getpatientwithnoteaminfo"),
+    url(r'^add_patient_to_team/', ManageTeam.addpatienttoteam, name="ManageTeam_addpatienttoteam"),
+    url(r'^reload_team_data/', ManageTeam.reloadteamdata, name="ManageTeam_reloadteamdata"),
+    url(r'^remove_team/', ManageTeam.removeteam, name="ManageTeam_removeteam"),
+    url(r'^add_team/', ManageTeam.addteam, name="addteam"),
+    url(r'^addteamtodb/', ManageTeam.addteamtodb, name="addteamtodb")
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 ]
--- OKare/OKareApp/views/ManageTeam.py
+++ OKare/OKareApp/views/ManageTeam.py
@@ -3,7 +3,19 @@ from OKareApp.models import Teams
 from OKareApp.models import Patient
 from OKareApp.models import Account
 from django.http import JsonResponse
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 from django.core import serializers
+=======
+from django.http import HttpResponse
+from django.template import loader
+
+>>>>>>> Stashed changes
+=======
+from django.http import HttpResponse
+from django.template import loader
+
+>>>>>>> Stashed changes
 import json
 
 
@@ -18,6 +30,48 @@ def index(request):
     # return HttpResponse(leads_as_json, content_type='json')
     return render(request, 'administrator/manageteam.html', context)
     pass
+
+
+def removeteam(request):
+    team_id = request.POST.get("teamid")
+    print(team_id)
+    patient_objs = Patient.objects.filter(team_id=team_id)
+    for patient in patient_objs:
+        patient.team_id = None
+        patient.bed = None
+        patient.ward = None
+        patient.save()
+
+    nurs_objs = Account.objects.filter(team_id=team_id, type="Nurse")
+    for nurse in nurs_objs:
+        nurse.team_id = None
+        nurse.save()
+
+    team_inst = Teams.objects.filter(id=team_id)
+    team_inst.delete()
+
+    return HttpResponse("Success")
+    pass
+
+
+def reloadteamdata(request):
+    id = 1
+
+    all_teams = Teams.objects.all()
+
+    response_data = {}
+    team_info = []
+    for team in all_teams:
+        record = {"id": team.id,
+                  "name": team.name,
+                  "ward": team.ward}
+        team_info.append(record)
+
+    response_data["data"] = team_info
+    print("i am in")
+    return JsonResponse(json.dumps(response_data), safe=False)
+    pass
+
 
 
 def returnteaminfo(request):
@@ -50,3 +104,65 @@ def returnteaminfo(request):
 
 
 
+<<<<<<< Updated upstream
+=======
+    return HttpResponse("success")
+    pass
+
+
+def addteam(request):
+    template = loader.get_template('administrator/add_team.html')
+    page_name = 'Add Team'    #Fill in here
+
+
+    context = {
+        'page_name': page_name,
+
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def addteamtodb(request):
+    print("TEST")
+    if request.POST:
+        print("TEST2")
+        ward_no = request.POST['ward_no']
+        team_name = request.POST['team_name']
+        print(ward_no)
+
+        team = Teams(name=team_name, ward=ward_no)
+
+<<<<<<< Updated upstream
+        team.save()
+        return HttpResponse('successful')
+>>>>>>> Stashed changes
+=======
+    return HttpResponse("success")
+    pass
+
+
+def addteam(request):
+    template = loader.get_template('administrator/add_team.html')
+    page_name = 'Add Team'    #Fill in here
+
+
+    context = {
+        'page_name': page_name,
+
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def addteamtodb(request):
+    print("TEST")
+    if request.POST:
+        print("TEST2")
+        ward_no = request.POST['ward_no']
+        team_name = request.POST['team_name']
+        print(ward_no)
+
+        team = Teams(name=team_name, ward=ward_no)
+
+        team.save()
+        return HttpResponse('successful')
+>>>>>>> Stashed changes
